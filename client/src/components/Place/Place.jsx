d0ef1f826dd12173b6f9f3368f8ddb40704085d7
Facupelli <facundopellicer4@gmail.com>
2022-03-16 11:31:41 -0300

    drone pics

--- client/src/components/Place/Place.jsx
+++ client/src/components/Place/Place.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import introV from "../../media/introV.jpeg";
 import introH from "../../media/introH.jpeg";
+import complejo from '../../media/droneResized/comple2.jpg'
 
 import s from "./Place.module.scss";
 
@@ -8,7 +9,7 @@ export const Place = () => {
   return (
     <div className={s.container}>
       <p className={s.text}>ALALA CAMPO VENI A DESCANSAR</p>
-      <img src={introH} alt="place" className={s.image} />
+      <img src={complejo} alt="place" className={s.image} />
       <img src={introV} alt="place2" className={s.imageV} />
     </div>
   );
--- client/src/utils/comunPhotos.js
+++ client/src/utils/comunPhotos.js
@@ -7,16 +7,26 @@ import comun9 from "../media/resized/comunResized/comunRe9.jpg";
 import comun10 from "../media/resized/comunResized/comunRe10.jpg";
 import comun24 from "../media/resized/comunResized/comunRe24.jpg";
 import comun11 from "../media/resized/comunResized/comunRe11.jpg";
-import comun14 from "../media/resized/comunResized/comunRe14.jpg";
 import comun15 from "../media/resized/comunResized/comunRe15.jpg";
 import comun16 from "../media/resized/comunResized/comunRe16.jpg";
 import comun21 from "../media/resized/comunResized/comunRe21.jpg";
-import comun22 from "../media/resized/comunResized/comunRe22.jpg";
-import comun23 from "../media/resized/comunResized/comunRe23.jpg";
 import comun26 from "../media/resized/comunResized/comunRe26.jpg";
 import comun27 from "../media/resized/comunResized/comunRe27.jpg";
+import drone1 from "../media/droneResized/dosCasas1.jpg";
+import drone2 from "../media/droneResized/dosCasas3.jpg";
+import drone3 from "../media/droneResized/DJI_0838.jpg";
+import drone4 from "../media/droneResized/DJI_0840.jpg";
+import drone5 from "../media/droneResized/araucoVolley1.jpg";
+
+
+
 
 export const comunImages = [
+  drone1,
+  drone2,
+  drone3,
+  drone4,
+  drone5,
   comun1,
   comun2,
   comun3,
@@ -26,12 +36,9 @@ export const comunImages = [
   comun10,
   comun24,
   comun11,
-  comun14,
   comun15,
   comun16,
   comun21,
-  comun22,
-  comun23,
   comun26,
   comun27,
 ];
