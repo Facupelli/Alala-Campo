721685400457c2c7c2ad15adcdf72e4e58a44e5a
Facupelli <facundopellicer4@gmail.com>
2022-03-17 17:30:35 -0300

    change responsive photo

--- client/src/components/Place/Place.jsx
+++ client/src/components/Place/Place.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import introV from "../../media/introV.jpeg";
-import introH from "../../media/introH.jpeg";
 import complejo from '../../media/droneResized/comple2.jpg'
 
 import s from "./Place.module.scss";
@@ -10,7 +9,7 @@ export const Place = () => {
     <div className={s.container}>
       <p className={s.text}>ALALA CAMPO VENI A DESCANSAR</p>
       <img src={complejo} alt="place" className={s.image} />
-      <img src={introV} alt="place2" className={s.imageV} />
+      <img src={complejo} alt="place2" className={s.imageV} />
     </div>
   );
 };
